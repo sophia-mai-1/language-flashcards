56846a29253230a8caf48a8f046d8ff915d15f3b
Step <stphnlines@gmail.com>
2024-11-25 20:58:56 -0800

    started pseudocode

---
+++ functions.py
@@ -0,0 +1,22 @@
+#importing all necessary APIs
+
+
+
+def userInput():
+    #prompts user to input word that they want to convert to flashcard
+
+    #(this may be more of a form intake thing on the html side)
+    #takes input and preps it to be sent to Google Translate API translate method
+    #stores input word as a global variable
+
+def trasnlateInput():
+    #requests google translate API to translate word from english to chinese
+    #returns the translated output
+
+def getExamples():
+    #access openAI API to prompt a response that would give examples for the word at hand.
+
+
+
+
+
